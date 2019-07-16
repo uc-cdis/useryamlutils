f414b52254dcf5336ffdfc83914e2526264f1bf9
Giang Bui <giangbui0816@gmail.com>
2019-07-16 14:50:24 -0500

    feat(click): use click

--- main.py
+++ main.py
@@ -1,29 +1,33 @@
+import click
 from utils import add_policy_to_all_users
 
-ALL_DEV_USERS = [
+ALL_PLAX_DEV_USERS = [
     "abbygeorge@uchicago.edu",
     "aprokh@uchicago.edu",
     "atharvar@uchicago.edu",
     "avantol@uchicago.edu",
-    "dgrantkeane@uchicago.edu",
     "diw@uchicago.edu",
-    "dmiller15@uchicago.edu",
     "emalinowski@uchicago.edu",
     "fantix@uchicago.edu",
     "fauzi@uchicago.edu",
-    "fitz@uchicago.edu",
     "giangbui@uchicago.edu",
     "mlukowski@uchicago.edu",
     "mshao1@uchicago.edu",
-    "mikeabreu@uchicago.edu",
     "qshu@uchicago.edu",
     "reubenonrye@uchicago.edu",
     "ribeyre@uchicago.edu",
     "rudyardrichter@uchicago.edu",
     "thanhnd@uchicago.edu",
     "vzpgb@uchicago.edu",
+    "yajingt@uchicago.edu"
+]
+
+ALL_DEV_USERS = ALL_PLAX_DEV_USERS + [
+    "dgrantkeane@uchicago.edu",
+    "dmiller15@uchicago.edu",
+    "fitz@uchicago.edu",
+    "mikeabreu@uchicago.edu",
     "kuangx@uchicago.edu",
-    "yajingt@uchicago.edu",
     "zgowani@uchicago.edu",
     "zlchitty@uchicago.edu"]
 
@@ -35,6 +39,7 @@ ALL_BIO_USERS = [
 
 ALL_COMMONS = ["accountprod",
                "anvil",
+               "anvilstaging",
                "bpa",
                "canine",
                "cvb",
@@ -50,12 +55,17 @@ ALL_COMMONS = ["accountprod",
                "stagingdatastage",
                "vadcprod"]
 
-def add_policy_for_all_dev_in_all_commons(policy):
-
-    root = "/Users/giangbui/Projects/commons-users/users"
+def add_policy_for_users(root, users, policy):
+    """
+    Add policy
+    :param root: path to common users. Ex. /Users/giangbui/Projects/commons-users/user
+    :param users: list of users
+    :param policy: policy
+    :return:
+    """
 
     for common in ALL_COMMONS:
-        add_policy_to_all_users(policy, ALL_DEV_USERS, root + "/" + common + "/user.yaml")
+        add_policy_to_all_users(policy, users, root + "/" + common + "/user.yaml")
 
 
 def remove_user_from_all_commons(user):
@@ -66,9 +76,19 @@ def add_user_to_common(user, user_yaml, json_block):
     raise NotImplementedError
 
 
-def main():
-    add_policy_for_all_dev_in_all_commons("prometheus")
+@click.group()
+def cli1():
+    pass
 
 
+@cli1.command()
+@click.option('--root', required=True, help='path to common users dir')
+@click.option('--policy', required=True, help='policy')
+def add_policy(root, policy):
+    """add policy."""
+    add_policy_for_users(root, policy, ALL_DEV_USERS)
+
+
+cli = click.CommandCollection(sources=[cli1])
 if __name__ == "__main__":
-    main()
+    cli()
